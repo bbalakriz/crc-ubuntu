eb0e1b86fa7a3f43c66337182f5e8373b3f3e39d
bbalakriz <37283315+bbalakriz@users.noreply.github.com>
2020-03-06 14:30:03 +0800

    Create chrome-desktop-vm.sh

---
+++ chrome-desktop-vm.sh
@@ -0,0 +1,14 @@
+#on the gcp ubuntu - nexted virtualization enabled vm, run the following
+
+wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
+sudo apt update
+sudo dpkg --install chrome-remote-desktop_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
+sudo DEBIAN_FRONTEND=noninteractive apt install --assume-yes xfce4 desktop-base
+
+sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
+sudo apt install --assume-yes xscreensaver
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+
+sudo dpkg --install google-chrome-stable_current_amd64.deb
+sudo apt install --assume-yes --fix-broken
