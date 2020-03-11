0bf2c5b9490bbab8c1f2fb1baaf2557cbb2484f4
bbalakriz <37283315+bbalakriz@users.noreply.github.com>
2020-03-11 17:50:45 +0800

    Update 3-chrome-desktop-vm.sh

--- 3-chrome-desktop-vm.sh
+++ 3-chrome-desktop-vm.sh
@@ -4,12 +4,14 @@ wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo apt update
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
-sudo DEBIAN_FRONTEND=noninteractive apt install --assume-yes xfce4 desktop-base
 
+#install xfce
+sudo DEBIAN_FRONTEND=noninteractive apt install --assume-yes xfce4 desktop-base
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'
 sudo apt install --assume-yes xscreensaver
-wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
+#install chrome
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 
